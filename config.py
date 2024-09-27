dc4c8d65344c09cf5ae03c39884d036a6234442a
Abdur rehman <110470885+Abdurrehman0815@users.noreply.github.com>
2024-09-27 11:46:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-SMTP_EMAIL = "mrbalajibtech@gmail.com"
-SMTP_APP_PASSWORD = "pjpp jmng jikh vzdq"
+SMTP_EMAIL = "your mail id"
+SMTP_APP_PASSWORD = "app password create by your email id"
